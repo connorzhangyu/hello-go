6bb2ba50eb4fd34055c0c9ca64ecf88b598cabbd
connor <88358827+connorzhangyu@users.noreply.github.com>
2025-08-08 23:03:56 +0700

    Create hello.go

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package hello
+
+import "fmt"
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func Hello() {
+	fmt.Println("Hello Go!")
+}
